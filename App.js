c92c09574106d648f78b2d25c649fe9a42f7bfa9
kacper.sledz <casper.sledx@gmail.com>
2019-01-26 09:26:03 +0100

    basic navigation

--- App.js
+++ App.js
@@ -8,11 +8,31 @@
  */
 
 import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Detail from './src/detail';
 import Main from './src/main';
 
-type Props = {};
-export default class App extends Component<Props> {
+const AppNavigator = createStackNavigator(
+  {
+    Main: {
+      screen: Main
+    },
+    Details: {
+      screen: Detail
+    }
+  },
+  {
+    initialRouteName: 'Main'
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return <Main />;
+    return <AppContainer />;
   }
 }
